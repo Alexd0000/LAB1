23f9f8c999f512976e98455312d5cbf1fca0965b
Allll Dllll <Allll Dllll>
2016-12-09 15:50:54 +0100

    hello

--- Project/Calculator.hs
+++ Project/Calculator.hs
@@ -16,6 +16,7 @@ import Expr
 canWidth  = 300
 canHeight = 300
 
+-- Function that reads the expression from the given input element and draws the graph on the given canvas
 readAndDraw :: Elem -> Canvas -> IO ()
 readAndDraw input can = do
                          stringExpr <- getProp input "value"
@@ -25,6 +26,13 @@ readAndDraw input can = do
 -- render : Clear a canvas, then draw a picture onto it.
 -- stroke : Draw the contours of a shape.
 
+showAndDrawDiff :: Elem -> Canvas -> IO()
+showAndDrawDiff input can = do
+                              stringExpr <- getProp input "value"
+                              case (readExpr stringExpr) of
+                                Just expr  -> let diffExpr = differentiate expr in do setProp input "value" (showExpr diffExpr)
+                                                                                      render can (stroke (path (points diffExpr 0.04 (canWidth, canHeight))))
+                                Nothing -> setProp input "value" "Err : Wrong expression"
 main = do
     -- Elements
     canvas  <- mkCanvas canWidth canHeight   -- The drawing area
@@ -53,6 +61,7 @@ main = do
     Just can <- getCanvas canvas
     onEvent draw  Click $ \_    -> readAndDraw input can
     onEvent input KeyUp $ \code -> when (code==13) $ readAndDraw input can
+    onEvent diff Click $ \_ -> showAndDrawDiff input can
       -- "Enter" key has code 13
 
 
